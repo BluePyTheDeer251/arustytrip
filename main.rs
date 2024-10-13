e3edcd3f75903592d64846a3d2b1c49d25468b77
BluePy <bluepythewolfdeer@proton.me>
2024-10-13 10:13:18 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,4 +1,7 @@
 fn main() {
-    println!("Hello, I am BluePy's Rust experiment, he might update me in the future.")
-    thing = ""
+    println!("Hello, I am BluePy's Rust experiment, he might update me in the future.");
+    let thing = "test";
+    let mut thing2 = "test2";
+    println!("{test}", "{test2}");
+    
 }
