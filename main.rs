a07cc1e280b8b6da8e7d138f88d602d271ed385d
Paul <bluepythewolfdeer@proton.me>
2024-10-13 10:10:45 -0600

    Learned strings

--- main.rs
+++ main.rs
@@ -1,4 +1,5 @@
 fn main() {
-    println!("Hello, I am BluePy's Rust experiment, he might update me in the future.")
-    thing = ""
+    println!("Hello, I am BluePy's Rust experiment, he might update me in the future.");
+    let thing = "Test";
+    println!("{thing}");
 }
