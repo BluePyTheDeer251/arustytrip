352af147aab5e9121dfb79fffe78bc7f137ad0f0
Paul <bluepythewolfdeer@proton.me>
2024-11-02 15:23:11 -0600

    Added and broke

--- main.rs
+++ main.rs
@@ -2,8 +2,10 @@ fn main() {
     println!("Hello, I am BluePy's Rust experiment, he might update me in the future.");
     let thing = "Test";
     println!("{thing}");
-    let thing = "test";
-    let mut thing2 = "test2";
-    println!("{test}", "{test2}");
-    thing = "test3"
+    let mut thing = "test";
+    let thing2 = "test2";
+    println!("{thing}, {thing2}");
+    thing = "test3";
+    println!("{thing}");
+    println!("This is slow...");
 }
